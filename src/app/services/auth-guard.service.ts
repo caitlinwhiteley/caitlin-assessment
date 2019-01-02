cc17dd7ea078bf371bca147674d5d52fff343e44
Caitlin Whiteley <Caitlin.Whiteley@nology.io>
2019-01-02 15:15:38 +0000

    auth guard service set up

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router, CanActivate } from '@angular/router';
+import { Observable } from 'rxjs';
+import { first, map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(
+    private firebaseAuth: AngularFireAuth, 
+    private router: Router
+  ) { }
+
+  public canActivate(): Observable<boolean> {
+    return this.firebaseAuth.authState.pipe(
+      map((user) => {
+        if (user) {
+          return true;
+        } else {
+          this.router.navigate(['login']);
+          return false;
+        }
+      },
+      ),
+      first(),
+    );
+  }
+
 }
