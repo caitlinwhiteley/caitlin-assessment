bfc1f407630d25d39375dd0c6b046de732e53832
Caitlin Whiteley <Caitlin.Whiteley@nology.io>
2019-01-04 11:44:44 +0000

    fixed responsive issues and edited register and login

--- src/app/forgotten-password/forgotten-password.component.ts
+++ src/app/forgotten-password/forgotten-password.component.ts
@@ -7,18 +7,18 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./forgotten-password.component.css']
 })
 export class ForgottenPasswordComponent implements OnInit {
-  hideMessage: boolean = true;
+  hideMessage = true;
 
   constructor(private authService: AuthService) {}
 
   resetPassword(data: {email: string}) {
-    console.log(data.email)
+    console.log(data.email);
     this.authService.resetPassword(data.email)
       .then(() => {
         this.hideMessage = false;
-        console.log("Email reset sent");
+        console.log('Email reset sent');
       })
-      .catch((error) => console.log("Error resetting password: " + error));
+      .catch((error) => console.log('Error resetting password: ' + error));
   }
 
   ngOnInit() {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -54,6 +54,16 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-  }
+  const countDownDate = new Date('Dec 25, 2019 00:00:00').getTime();
+  const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById('countdown').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+  }, 1000);
+}
 
 }
